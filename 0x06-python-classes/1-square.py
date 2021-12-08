a6f4f410804804154fd8b0c8142e827d1f934fda
Kimae <kartelbwoy@gmail.com>
2021-12-08 09:44:23 -0800

    Initial commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,17 @@ Defines class Square with private attribute size
 
 
 class Square:
+    """
+    class Square defination
+
+    Args:
+        size : size of a side of a square
+    """
     def __init__(self, size):
+    """
+    Initalize square
+
+    Attributes:
+        size : size of a side of a square
+    """
         self.__size = size

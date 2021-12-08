8f23cb11255678477155791a1c1df0b7e2dcd7dc
Kimae <kartelbwoy@gmail.com>
2021-12-08 10:08:45 -0800

    Initial commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module 3-square
+Defines class Square with private instance attribute size and public instance method area
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of a side in square
+
+    Functions:
+        __init__(self, size)
+        area(self)
+    """
+    def __init__(self, size=0):
+        """
+        Initializes square
+
+        Attributes:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+        Calculates the current square area
+
+        Returns:
+            square area
+        """
+        return (self.__size)**2

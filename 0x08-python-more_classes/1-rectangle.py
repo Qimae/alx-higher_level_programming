2b63ad9f154ee12455d4f5727236ea3c59e6fb52
Kimae <kartelbwoy@gmail.com>
2021-12-13 07:50:01 -0800

    property fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,13 +5,20 @@ Defines class Rectangle with private attribute width and height
 """
 
 
-class Rectangle():
+class Rectangle:
     """
     Defines a rectangle
 
-    Attributes:
-        width: width of rectangle
-        height: height of rectangle
+   Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
     """
     def __init__(self, width=0, height=0):
         """
@@ -20,12 +27,14 @@ class Rectangle():
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         """
         retrieves the width of a rectangle
         """
         return self.__width
-
+    
+    @width.setter
     def width(self, value):
         """
         sets the width of a rectangle
@@ -36,12 +45,14 @@ class Rectangle():
             raise ValueError("width must be >= 0")
         self.__width = value
     
+    @property
     def height(self):
         """
         retrieves the height of a rectangle
         """
         return self.__height
     
+    @height.setter
     def height(self, value):
         """
         sets the height of a rectangle

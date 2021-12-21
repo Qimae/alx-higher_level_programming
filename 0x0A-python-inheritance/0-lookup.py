3ae8ebceefbacb53a16aa8fa15f5db3cf2887274
Kimae <kartelbwoy@gmail.com>
2021-12-21 00:21:45 -0800

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup
+returns the list of available attributes, methods of object
+"""
+
+
+def lookup(obj):
+    """
+    returns list of object
+    """
+    return dir(obj)

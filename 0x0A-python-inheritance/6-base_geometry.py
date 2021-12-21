31a8e89fae66a73fa79cab7eee2206fc9862a9e4
Kimae <kartelbwoy@gmail.com>
2021-12-21 04:30:19 -0800

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        """
+        not implemented
+        """
+        raise Exception("area() is not implemented")

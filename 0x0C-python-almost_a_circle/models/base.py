cc753df032e6a1057eac3382b8e23409f0a0b38c
Kimae <kartelbwoy@gmail.com>
2021-12-30 23:09:16 -0800

    Initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module base
+
+Defines class Base with private attribute __nb_objects = 0
+with class constructor
+"""
+
+
+class Base:
+    """
+    Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes id
+        """
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

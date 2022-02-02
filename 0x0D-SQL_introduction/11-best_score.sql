7b57fe25e988927cedcb834420cd46c372d7fadf
Qimae <kartelbwoy@gmail.com>
2022-02-02 07:48:29 -0800

    Keyword fix

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists al records with score >= 10 from second_table
-SELECT score, name FROM second_table where score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

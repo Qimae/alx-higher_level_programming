2f40debcb61183551b77e577dc45bb5383696385
Qimae <kartelbwoy@gmail.com>
2022-03-18 07:36:13 -0700

    wc fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-# Script that takes in a URL sends a request to that URL and displays
-# the size of the body of the response
-# Usage: ./0-body_size.sh 0.0.0.0:5000
-
+#!/usr/bin/bash
+# Script displays size of body of reponse Usage: ./0-body_size.sh 0.0.0.0:5000
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '

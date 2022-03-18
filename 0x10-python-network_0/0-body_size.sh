93e399e9eb818c8ad096ccf668750059f75a313b
Qimae <kartelbwoy@gmail.com>
2022-03-18 07:33:51 -0700

    Initial commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Script that takes in a URL sends a request to that URL and displays
+# the size of the body of the response
+# Usage: ./0-body_size.sh 0.0.0.0:5000
+
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '

f620e6d069ea484be491799a10888a023c70c1eb
Qimae <kartelbwoy@gmail.com>
2022-03-24 22:07:13 -0700

    PEP validation fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ Script fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-if __name__=="__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
+if __name__== "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
